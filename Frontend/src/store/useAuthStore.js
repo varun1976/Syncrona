b307891141cab83ac9b2fb14b3d00404ccaa0dd3
varun1976 <kondreddyvarunkumar@gmail.com>
2026-07-31 21:21:45 +0530

    Bug Fixing

--- Frontend/src/store/useAuthStore.js
+++ Frontend/src/store/useAuthStore.js
@@ -18,15 +18,17 @@ export const useAuthStore = create((set, get) => ({
             set({ authUser: res.data });
             get().connectSocket();
         } catch (error) {
-            console.log("Error in checkAuth:", error);
             if (error.response?.status === 401) {
                 localStorage.removeItem("token");
+            } else {
+                console.log("Error in checkAuth:", error);
             }
             set({ authUser: null });
         } finally {
             set({ isCheckingAuth: false });
         }
     },
+
     signup: async (data) => {
         set({ isSigningUp: true });
         try {
